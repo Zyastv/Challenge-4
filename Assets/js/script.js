377deeb944b1f7227e359cd343c79add24ff51be
Zachary Adams <Zach2vegas@gmail.com>
2022-08-25 21:19:13 -0700

    Adding functions in javascript

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,8 +1,8 @@
 // HIGHSCORES BUTTON/MODAL
-var highscoresBtn = document.getElementById("highscores")
-var modal = document.getElementById("modal")
-var modalContent = document.getElementById("modal-content")
-var scores = document.getElementById("scores")
+var highscoresBtn = document.getElementById("highscores");
+var modal = document.getElementById("modal");
+var modalContent = document.getElementById("modal-content");
+var scores = document.getElementById("scores");
 
 highscoresBtn.onclick = function() {
     modal.style.display = "block";
@@ -14,4 +14,30 @@ window.onclick = function(event) {
     }
   }
 
-// 
+// QUIZ START
+var startBtn = document.getElementById("start-button");
+var question = document.getElementById("question");
+var info = document.getElementById("info");
+
+startBtn.onclick = function() {
+  startBtn.style.display = "none";
+  info.style.display = "none";
+  startquiz()
+}
+
+function startquiz(){
+var timeEl = document.querySelector(".time");
+var secondsLeft = 75;
+  var timerInterval = setInterval(function() {
+    secondsLeft--;
+    timeEl.textContent = secondsLeft;
+
+    if(secondsLeft === 0) {
+      clearInterval(timerInterval);
+      endquiz();
+    }
+
+  }, 1000);
+}
+
+
